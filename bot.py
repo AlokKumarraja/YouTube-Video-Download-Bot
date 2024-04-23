c387e0daa8fda5270b434fa5c4061a476f70a31a
AlokKumarraja <107189611+AlokKumarraja@users.noreply.github.com>
2024-04-23 23:41:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ import asyncio
 
 # Replace 'YOUR_API_ID', 'YOUR_API_HASH', and 'YOUR_BOT_TOKEN' with your actual values
 
-API_ID = ''
-API_HASH = ''
-BOT_TOKEN = ''
+API_ID = '12380656'
+API_HASH = 'd927c13beaaf5110f25c505b7c071273'
+BOT_TOKEN = '6939672699:AAG_Q_iI2CE9rqU88H18sqAGYEBgNGmAoX0'
 
 # Create a Pyrogram client
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
